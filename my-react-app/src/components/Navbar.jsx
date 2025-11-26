da7d582a71be28d747c94c17762294b8da7229dd
FernandoLpz0911 <FernandoLpz0911.business@Gmail.com>
2025-11-26 10:16:13 -0600

    Trying new things out

--- my-react-app/src/components/Navbar.jsx
+++ my-react-app/src/components/Navbar.jsx
@@ -1,25 +1,47 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
-
 import './cssfiles/navbar.css';
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <div className="nav-left-group">
-          <Link to ="/" className="nav-link">HOME</Link>
-          <Link to="/FoodDisplay" className="nav-link">BAKED GOODS</Link>
-        </div>
-
-        <Link to="/">
+        <Link to="/" className="bakery-logo">
           <img src="/images/BakeryLogo.jpg" alt="Dulce Tentaciones Bakery Logo" className="bakery-image" />
         </Link>
+        
+        {/* Hamburger menu button for mobile */}
+        <div className="menu-toggle" onClick={toggleMenu}>
+          <div className="menu-icon"></div>
+          <div className="menu-icon"></div>
+          <div className="menu-icon"></div>
+        </div>
 
-        <div className="nav-right-group"> 
+        {/* Desktop links */}
+        <div className="nav-links-desktop">
+          <div className="nav-left-group">
+            <Link to="/" className="nav-link">HOME</Link>
+            <Link to="/FoodDisplay" className="nav-link">BAKED GOODS</Link>
+          </div>
+          <div className="nav-right-group">
             <Link to="/About" className="nav-link">ABOUT US</Link>
             <Link to="/Contact" className="nav-link">CONTACT</Link>
+          </div>
         </div>
-        
+
+        {/* Mobile menu, conditionally rendered */}
+        <div className={`mobile-nav-links ${isMenuOpen ? 'active' : ''}`} onClick={toggleMenu}>
+          <Link to="/" className="nav-link">HOME</Link>
+          <Link to="/FoodDisplay" className="nav-link">BAKED GOODS</Link>
+          <Link to="/About" className="nav-link">ABOUT US</Link>
+          <Link to="/Contact" className="nav-link">CONTACT</Link>
+        </div>
       </div>
     </nav>
   );
